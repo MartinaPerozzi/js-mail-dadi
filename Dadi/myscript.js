747c5c81ba3d74d98a9f9af233d17d905a864996
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-26 16:18:15 +0100

    html + css + js- dadi

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -1,25 +1,31 @@
 // Gioco dei dadi
 
-// Numero random per pc
-const randomNumberMy = Math.floor(Math.random() * 6);
-console.log(randomNumberMy);
-// Numero random per user
-const randomNumberUser = Math.floor(Math.random() * 6);
-console.log(randomNumberUser);
 // Creo un'arena
 const containerPlayerOne = document.querySelector(".container-players>div");
 
 const containerPlayerTwo = document.querySelector(".container-players>div:last-child");
 
+// Numero random per pc
+const randomNumberMy = Math.floor(Math.random() * 6);
+console.log(randomNumberMy);
+containerPlayerOne.querySelector(".winner-or").innerHTML += randomNumberMy;
+
+// Numero random per user
+const randomNumberUser = Math.floor(Math.random() * 6);
+console.log(randomNumberUser);
+containerPlayerTwo.querySelector(".winner-or").innerHTML += randomNumberUser;
+
 
 // Partita
+
+
 if (randomNumberMy > randomNumberUser) {
 
-    containerPlayerOne.innerHTML += "Game over";
+    containerPlayerTwo.innerHTML += "Game Over";
 
 } else if (randomNumberMy < randomNumberUser) {
 
-    containerPlayerTwo.innerHTML += "Game over";
+    containerPlayerOne.innerHTML += "Game Over";
 }
 
 
