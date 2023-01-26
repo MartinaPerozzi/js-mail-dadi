0b3834b927fe620da237aa76b03a6b29142119a1
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-26 16:32:04 +0100

    js- dadi

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -6,7 +6,8 @@ const containerPlayerOne = document.querySelector(".container-players>div");
 const containerPlayerTwo = document.querySelector(".container-players>div:last-child");
 
 // Creo il bottone per giocare
-const playButton = document.querySelector("button-play>input");
+const playButton = document.querySelector(".button-play>input");
+
 // Numero random per pc
 const randomNumberMy = Math.floor(Math.random() * 6);
 console.log(randomNumberMy);
@@ -26,6 +27,24 @@ if (randomNumberMy > randomNumberUser) {
 
     containerPlayerOne.innerHTML += "Game Over";
 }
+
+playButton.addEventListener(
+    "click",
+    function () {
+
+        const randomNumberMy = Math.floor(Math.random() * 6);
+        const randomNumberUser = Math.floor(Math.random() * 6);
+
+        if (randomNumberMy > randomNumberUser) {
+
+            containerPlayerTwo.innerHTML = "Game Over";
+
+        } else if (randomNumberMy < randomNumberUser) {
+
+            containerPlayerOne.innerHTML = "Game Over";
+        }
+    }
+)
 
 
 
